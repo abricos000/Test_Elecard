2ceb9263b1c60467f7f416568ab672f3c3168480
abricos000 <manv_v@mail.ru>
2022-11-02 09:00:40 +0700

    refactor: function name changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -36,7 +36,7 @@ export function Main({ onRenderMethod }) {
           onMainPost={mainPost}
         />
       ) : (
-        <PageTreeList posts={posts} />
+        <PageTreeList onPosts={posts} />
       )}
     </main>
   );
